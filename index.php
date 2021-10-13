2c5ff242cf637dfce3711649f2bd99e2badb2945
Aurelie-dana <79651826+Aurelie-dana@users.noreply.github.com>
2021-10-13 15:21:04 +0200

    add index.php

---
+++ index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic">
+    <link rel="stylesheet" href="/fonts/font-awesome.css">
+    <link rel="stylesheet" href="/fonts/simple-line-icons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-dark navbar-expand-md sticky-top bg-dark" id="mainNav" style="background: #ffffff;">
+        <div class="container"><a class="navbar-brand" href="#">Accueil</a><button data-bs-toggle="collapse" class="navbar-toggler navbar-toggler-right" data-bs-target="#navbarResponsive" type="button" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation" value="Menu"><i class="fa fa-book"></i></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item nav-link"><a class="nav-link" href="register.php">S'inscrire</a></li>
+                    <li class="nav-item nav-link"><a class="nav-link" href="login.php">Se connecter</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <header class="text-center text-white masthead" style="background: #579DD7;">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-9 mx-auto position-relative">
+                    <h1 class="mb-5" style="font-size: 45px;">Bienvenue à la Médiathèque d'Alexandrie de La Chapelle-Curreaux</h1>
+                </div>
+                <div class="col-md-10 col-lg-8 col-xl-7 mx-auto position-relative">
+                    <form>
+                        <div class="row">
+                            <div class="col-12 col-md-9 mb-2 mb-md-0"><input class="form-control form-control-lg" type="email" placeholder="Pour vous inscrire cliquez sur le bouton -->"></div>
+							<a class="col-12 col-md-3 btn btn-primary btn-lg" role="button" href="/back-office/création-de-compte.html">S'inscrire</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </header>
+    <section class="text-center bg-light features-icons" style="background: url(&quot;/img/point.png&quot;);">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="mx-auto features-icons-item mb-5 mb-lg-0 mb-lg-3">
+                        <div class="d-flex features-icons-icon"><i class="icon-book-open m-auto text-primary" data-bss-hover-animate="pulse"></i></div>
+                        <h3><strong>Nos best-sellers&nbsp;&nbsp;</strong><br></h3>
+                        <p class="lead mb-0">Harry Potter et la coupe de feu<br></p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="mx-auto features-icons-item mb-5 mb-lg-0 mb-lg-3">
+                        <div class="d-flex features-icons-icon"><i class="icon-emotsmile m-auto text-primary" data-bss-hover-animate="pulse"></i></div>
+                        <h3><strong>Un large catalogue</strong><br></h3>
+                        <p class="lead mb-0">Vous n'aurez pas le temps de vous ennuyer !&nbsp;&nbsp;<br></p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="mx-auto features-icons-item mb-5 mb-lg-0 mb-lg-3">
+                        <div class="d-flex features-icons-icon"><i class="icon-check m-auto text-primary" data-bss-hover-animate="pulse"></i></div>
+                        <h3><strong>Facile d'acces</strong><br></h3>
+                        <p class="lead mb-0">Aussi simple qu'un jeux d'enfant!<br></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="showcase">
+        <div class="container-fluid p-0">
+            <div class="row g-0" style="background: #579DD7;">
+                <div class="col-lg-6 text-white order-lg-2 showcase-img" style="background: url(&quot;/img/Jules-Verne.jpg&quot;) center / auto no-repeat;"><span></span></div>
+                <div class="col-lg-6 my-auto order-lg-1 showcase-text">
+                    <h2>Roman d'aventure</h2>
+                    <p class="lead mb-0">Vous avez soif d'aventure ? Que vous ayez envie de relire le même livre ou de partir en quête de nouvelles aventure, vous trouverez sûrement de quoi vous évader !&nbsp;</p>
+                </div>
+            </div>
+            <div class="row g-0" style="background: url(&quot;/img/point.png&quot;);">
+                <div class="col-lg-6 text-white showcase-img" style="background: url(&quot;/img/Ender.jpg&quot;) center / auto no-repeat;"><span></span></div>
+                <div class="col-lg-6 my-auto order-lg-1 showcase-text">
+                    <h2>Roman de Science-Fiction&nbsp;<br></h2>
+                    <p class="lead mb-0">Envie de partir ailleurs ?&nbsp;Et pourquoi pas dans un autre monde ?&nbsp;Avec notre large choix de roman de Science-fiction, vous pouvez vous envoler dans l'espace et dans le temps.<br></p>
+                </div>
+            </div>
+            <div class="row g-0" style="background: #579DD7;">
+                <div class="col-lg-6 text-white order-lg-2 showcase-img" style="background: url(&quot;/img/Gatsby.jpg&quot;) center / auto no-repeat;"><span></span></div>
+                <div class="col-lg-6 my-auto order-lg-1 showcase-text">
+                    <h2>Roman d'amour</h2>
+                    <p class="lead mb-0">Envie de vous plonger dans un roman d'amour ?&nbsp;Aucun problème !&nbsp;Avec notre catalogue, le monde vous semblera rose.<br></p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="text-center bg-light testimonials" style="background: url(&quot;/img/point.png&quot;);">
+        <div class="container">
+            <h2 class="mb-5">Nos derniers livres</h2>
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="mx-auto testimonial-item mb-5 mb-lg-0"><img class="img-thumbnail img-fluid mb-3" src="/img/Pat.jpg">
+                        <h5><strong>Pat' Patrouille</strong><br></h5>
+                        <p class="font-weight-light mb-0"><strong>L'imagier des tout-petits</strong><br></p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="mx-auto testimonial-item mb-5 mb-lg-0"><img class="img-thumbnail img-fluid mb-3" src="/img/La%20Comptabilité.jpg">
+                        <h5><strong>La Comptabilité</strong><br></h5>
+                        <p class="font-weight-light mb-0"><strong>Tout-en-un pour les Nuls</strong><br></p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="mx-auto testimonial-item mb-5 mb-lg-0"><img class="img-thumbnail img-fluid mb-3" src="/img/histoire.jpg">
+                        <h5>Le Grand livre de l'histoire de france&nbsp;<br><br></h5>
+                        <p class="font-weight-light mb-0"></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer class="bg-light footer" style="background: linear-gradient(white 10%, rgb(110,170,220) 75%, #579DD7 89%);">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 text-center text-lg-start my-auto h-100" style="text-align: center;">
+                    <ul class="list-inline mb-2">
+                        <li class="list-inline-item"><a href="#"><br>Mentions légales<br></a></li>
+                        <li class="list-inline-item"><span>⋅</span></li>
+                        <li class="list-inline-item"><a href="#">Contact</a></li>
+                        <li class="list-inline-item"><span>⋅</span></li>
+                        <li class="list-inline-item"><a href="#"><br><br>Engagement de confidentialité<br></a></li>
+                        <li class="list-inline-item"></li>
+                        <li class="list-inline-item"></li>
+                    </ul>
+                    <p class="text-muted small mb-4 mb-lg-0">© La Médiathèque d'Alexandrie 2021. All Rights Reserved.</p>
+                </div>
+                <div class="col-lg-6 text-center text-lg-end my-auto h-100">
+                    <ul class="list-inline mb-0">
+                        <li class="list-inline-item"><a href="#"><i class="fa fa-facebook fa-2x fa-fw"></i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fa fa-twitter fa-2x fa-fw"></i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fa fa-instagram fa-2x fa-fw"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="/js/script.js"></script>
+    <script src="/js/bs-init.js"></script>
+</body>
+
+</html>
