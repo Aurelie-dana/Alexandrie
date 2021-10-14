cbe66c1d1b79c77ed9fc67c0e3bc3473af13565b
Aurelie-dana <79651826+Aurelie-dana@users.noreply.github.com>
2021-10-14 16:47:46 +0200

    add file

---
+++ admin/mot-de-passe-oublie.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Mot de passe oublié</title>
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
+    <link rel="stylesheet" href="/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="/fonts/fontawesome5-overrides.css">
+</head>
+
+<body class="bg-gradient-primary" style="background: #579DD7;">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-9 col-lg-12 col-xl-10">
+                <div class="card shadow-lg o-hidden border-0 my-5">
+                    <div class="card-body p-0">
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-flex" style="background: url(img/oublie.png) center / contain no-repeat, url(/img/point.png);">
+                                <div class="flex-grow-1 bg-password-image"></div>
+                            </div>
+                            <div class="col-lg-6" style="background: url(/img/point.png);">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h4 class="text-dark mb-2">Mot de passe oublié ?&nbsp;</h4>
+                                        <p class="mb-4">Oups !&nbsp;Vous avez oublié votre mot de passe ?&nbsp;Aucun souci, veuillez indiquer votre adresse mail et nous vous enverrons un nouveau mot de passe.&nbsp;<br></p>
+                                    </div>
+                                    <form class="user">
+                                        <div class="mb-3"><input class="form-control form-control-user" type="email" id="adresseEmail" aria-describedby="emailHelp" placeholder="Entrez votre adresse mail" name="adresseEmail"></div><button class="btn btn-primary d-block btn-user w-100" type="submit">Réinitialisation du mot de passe&nbsp;</button>
+                                    </form>
+                                    <div class="text-center">
+                                        <hr><a class="small" href="/register.php">Créer mon compte</a>
+                                    </div>
+                                    <div class="text-center"><a class="small" href="/login.php">Vous avez déjà un compte ? Identifier vous ici&nbsp;</a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="/js/script.js"></script>
+    <script src="/js/theme.js"></script>
+</body>
+
+</html>
